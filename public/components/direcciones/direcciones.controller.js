7811772d28ade7993522a796de4fa97045580318
Christine <cvarelaf@ucenfotec.ac.cr>
2018-03-15 20:49:52 -0600

    Registrar Direcciones

--- public/components/direcciones/direcciones.controller.js
+++ public/components/direcciones/direcciones.controller.js
@@ -42,5 +42,27 @@
       vm.listaDirecciones = servicioDirecciones.getDirecciones();
     }
 
+    // var firstOption = ["Heredia","San Jose"];
+
+    // var secondOption = [["Heredia","Barva","Santo Domingo","Santa BÃ¡rbara","San Rafael","San Isidro","BelÃ©n","Flores","San Pablo","SarapiquÃ­"],["San JosÃ©","EscazÃº","Desamparados","Puriscal","TarrazÃº","AserrÃ­", "Mora", "Goicoechea","Santa Ana","Alajuelita","VÃ¡squez de Coronado","Acosta","TibÃ¡s","Moravia","Montes de Oca", "Turrubares","Dota","Curridabat","PÃ©rez ZeledÃ³n","LeÃ³n CortÃ©s"]];
+
+    // var thirdOption = [["Heredia", "Mercedes", "San Francisco",  "Ulloa", "Varablanca"],["San JosÃ©","EscazÃº","Desamparados","Puriscal","TarrazÃº","AserrÃ­", "Mora", "Goicoechea","Santa Ana","Alajuelita","VÃ¡squez de Coronado","Acosta","TibÃ¡s","Moravia","Montes de Oca", "Turrubares","Dota","Curridabat","PÃ©rez ZeledÃ³n","LeÃ³n CortÃ©s"]];
+    
+    // function myCtrl{
+    //   $scope.options1 = firstOption;
+    //   $scope.options2 = []; // we'll get these later
+    //   $scope.getOptions2 = function(){
+    //       var key = $scope.options1.indexOf($scope.option1);
+    //       var myNewOptions = secondOption[key];
+    //       $scope.options2 = myNewOptions;
+    //   }
+    //   $scope.options3 = []; // we'll get these later
+    //   $scope.getOptions3 = function(){
+    //       var key = $scope.options2.indexOf($scope.option2);
+    //       var myNewOptions = thirdOption[key];
+    //       $scope.options3 = myNewOptions;
+    //   }
+    // }
+
   }
 })();
