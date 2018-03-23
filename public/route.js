314159f19342ec1e05e4c20d1df5d4ea923809c5
judithal <judith.al.al@gmail.com>
2018-03-23 14:23:09 -0600

    Merge Nicole

--- public/route.js
+++ public/route.js
@@ -317,6 +317,140 @@
       controllerAs: 'vm'
     })
 
+    .state('consultarBitacora', {
+      url: '/consultarBitacora',
+      templateUrl: './components/consultarBitacora/consultarBitacora.vista.html',
+      resolve:{
+        load: ['$ocLazyLoad', ($ocLazyLoad) => {
+          return $ocLazyLoad.load('./components/consultarBitacora/consultarBitacora.controlador.js')
+        }]
+      },
+      controller:'controladorBitacora',
+      controllerAs:'vm'
+    })
+    
+    .state('rankingTipoDeProductosProductos', {
+      url: '/rankingTipoDeProductosProductos',
+      templateUrl: './components/rankingProductos/ranking.vista.html',
+      resolve:{
+        load: ['$ocLazyLoad', ($ocLazyLoad) => {
+          return $ocLazyLoad.load('./components/rankingProductos/ranking.controlador.js')
+        }]
+      },
+      controller:'controladorRanking',
+      controllerAs:'vm'
+    })
+
+    .state('modificarTipoProducto', {
+      url: '/modificarTipoProducto',
+      templateUrl: './components/tipoDeProducto/modificarTipoProducto.vista.html',
+      params:{
+        nombreTipoProducto:''
+      },
+      resolve:{
+        load: ['$ocLazyLoad', ($ocLazyLoad) => {
+          return $ocLazyLoad.load('./components/tipoDeProducto/modificarTipoProducto.controlador.js')
+        }]
+      },
+      controller:'controladorModProducto',
+      controllerAs:'vm'
+    })
+
+    .state('registroTipoProducto', {
+      url: '/registrarTipoProducto',
+      templateUrl: './components/tipoDeProducto/tipoProducto.vista.html',
+      resolve:{
+        load: ['$ocLazyLoad', ($ocLazyLoad) => {
+          return $ocLazyLoad.load('./components/tipoDeProducto/tipoProducto.controlador.js')
+        }]
+      },
+      controller:'controladorProductos',
+      controllerAs:'vm'
+    })
+
+    .state('ListaTipoProducto', {
+      url: '/ListaTipoProducto',
+      templateUrl: './components/tipoDeProducto/listaTipoProductos.vista.html',
+      resolve:{
+        load: ['$ocLazyLoad', ($ocLazyLoad) => {
+          return $ocLazyLoad.load('./components/tipoDeProducto/tipoProducto.controlador.js')
+        }]
+      },
+      controller:'controladorProductos',
+      controllerAs:'vm'
+    })
+    
+    .state('listaProvincia', {
+      url: '/listaProvincia',
+      templateUrl: './components/provincia/listaProvincia.vista.html',
+      resolve:{
+        load: ['$ocLazyLoad', ($ocLazyLoad) => {
+          return $ocLazyLoad.load('./components/provincia/provincia.controlador.js')
+        }]
+      },
+      controller:'controladorProvincia',
+      controllerAs:'vm'
+    })
+
+    .state('registroProvincia', {
+      url: '/registrarProvincia',
+      templateUrl: './components/provincia/provincia.vista.html',
+      resolve:{
+        load: ['$ocLazyLoad', ($ocLazyLoad) => {
+          return $ocLazyLoad.load('./components/provincia/provincia.controlador.js')
+        }]
+      },
+      controller:'controladorProvincia',
+      controllerAs:'vm'
+    })
+    .state('registroCanton', {
+      url: '/registrarCanton',
+      templateUrl: './components/canton/canton.vista.html',
+      resolve:{
+        load: ['$ocLazyLoad', ($ocLazyLoad) => {
+          return $ocLazyLoad.load('./components/canton/canton.controlador.js')
+        }]
+      },
+      controller:'controladorCanton',
+      controllerAs:'vm'
+    })
+
+    .state('listaCantones', {
+      url: '/listaCantones',
+      templateUrl: './components/canton/listaCanton.vista.html',
+      resolve:{
+        load: ['$ocLazyLoad', ($ocLazyLoad) => {
+          return $ocLazyLoad.load('./components/canton/listaCanton.vista.html')
+        }]
+      },
+      controller:'controladorCanton',
+      controllerAs:'vm'
+    })
+
+    .state('listaCourier', {
+      url: '/listaCourier',
+      templateUrl: './components/courier/listaCourier.vista.html',
+      resolve:{
+        load: ['$ocLazyLoad', ($ocLazyLoad) => {
+          return $ocLazyLoad.load('./components/courier/courier.controlador.js')
+        }]
+      },
+      controller:'controladorCourier',
+      controllerAs:'vm'
+    })
+
+  .state('registroCourier', {
+    url: '/registrarCourier',
+    templateUrl: './components/courier/courier.vista.html',
+    resolve:{
+      load: ['$ocLazyLoad', ($ocLazyLoad) => {
+        return $ocLazyLoad.load('./components/courier/courier.controlador.js')
+      }]
+    },
+    controller:'controladorCourier',
+    controllerAs:'vm'
+  });
+
     .state('landing-page', {
       url: '/',
       templateUrl: './components/landingPage/landing.vista.html',
