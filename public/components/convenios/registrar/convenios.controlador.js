1b13543c4a5bdc99d471a0198f2fbb8133156763
Camila Gutiérrez <35516188+camiguti2996@users.noreply.github.com>
2018-03-23 20:35:32 -0600

    Ajustes a conveniosClientes
    Creación del servicio y ajustes en controladores y vistas de conveniosClientes

---
+++ public/components/convenios/registrar/convenios.controlador.js
@@ -0,0 +1,49 @@
+(() => {
+  'use strict';
+  angular
+  .module('correos-cr')
+  .controller('controladorConveniosCliente', controladorConveniosCliente);
+
+  controladorConveniosCliente.$inject = ['$stateParams', '$state', 'servicioConvenioClientes'];
+
+  function controladorConveniosCliente($stateParams, $state, servicioConvenioClientes){
+    let vm = this;
+
+    vm.nuevoConvenio = {};
+    listarConvenios();
+
+    // Funcion que es llamada desde el html para regustra un nuevo usuario
+    vm.registrarConvenio = (pnuevoConvenio) => {
+      
+      let idRandom = (Math.random()*Math.random())*1000;
+
+      console.log(idRandom);
+
+      // Tomamos el objeto sin formato y lo comvertimos en un objeto de tipo cliente
+      let objNuevoConvenio = new ConveniosClientes(pnuevoConvenio.servicio, pnuevoConvenio.cliente, pnuevoConvenio.direccion, idRandom);
+
+      console.log(objNuevoConvenio);
+
+      servicioConvenioClientes.agregarConvenioCliente(objNuevoConvenio);
+      
+      // Retroalimentacion Visual para los usuarios: SweetAlert
+      swal("Registro exitoso", "La nueva sucursal se ha sido registrado correctamente", "success", {
+        button: "Aceptar",
+      });
+
+      listarConvenios();
+
+      // Se limpia el formulario
+      vm.nuevoConvenio = null;
+    }
+
+    function listarConvenios(){
+      vm.listaConvClientes = servicioConvenioClientes.obtenerConvenioCliente();
+    }
+    
+    vm.modificar = (pconvenio) =>{
+      $state.go('modificarConvCliente', {idConvenio: JSON.stringify(pconvenio.idConvenio)})
+    }
+
+  }
+})(); 
